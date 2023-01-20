ddc6bd33e6d603e3d372a15652ec7a5cd2001873
gachagajnr <williampius17@users.noreply.github.com>
2023-01-20 11:21:32 +0300

    create layout for quick links

--- src/features/footer/footer.js
+++ src/features/footer/footer.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className="text-center">
+    <div className="text-center p-3">
       {"Copyright © "}
       <Link to="/" style={{ textDecoration: "none" }}>
         ArtyLand
--- src/features/subscribeform/subscribeform.js
+++ src/features/subscribeform/subscribeform.js
@@ -2,26 +2,37 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Stack from "react-bootstrap/Stack";
 import SocialIcons from "features/socialicons/socialicons";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function SubscribeForm() {
   return (
-    <div className="mt-2 d-flex justify-content-center">
-      <Stack direction="vertical">
-        <div className="mt-2 p-5 d-flex justify-content-center">
-          <SocialIcons />
-        </div>
-        <h5 className="text-center">Subscribe To Mailing List</h5>
-        <div className="mt-1 p-3 d-flex justify-content-center">
-          <Stack direction="horizontal" gap={2}>
-            <Form.Control
-              className="me-auto"
-              placeholder="Enter your email address"
-            />
-            <Button variant="primary">Subscribe</Button>
-          </Stack>
-        </div>
-      </Stack>
-    </div>
+    <Container>
+      <Row>
+        <Col sm={8}>
+          <div className="mt-2 p-5 d-flex justify-content-center">
+            QuickLinks
+          </div>
+        </Col>
+        <Col sm={4}>
+          <div className="mt-2 p-5 d-flex justify-content-center">
+            <SocialIcons />
+          </div>
+          <h5 className="text-center">Subscribe To Mailing List</h5>
+          <div className="mt-1 p-3 d-flex justify-content-center">
+            <Stack direction="horizontal" gap={2}>
+              <Form.Control
+                className="me-auto"
+                placeholder="Enter your email address"
+              />
+              <Button variant="primary">Subscribe</Button>
+            </Stack>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 export default SubscribeForm;
+
