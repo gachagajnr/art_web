0f0e5984baeb7fcc8deefb6d47a674ac2f088cd8
gachagajnr <williampius17@users.noreply.github.com>
2023-01-21 02:01:10 +0300

    art card improvements

--- src/features/art/artcard.js
+++ src/features/art/artcard.js
@@ -10,32 +10,33 @@ import { ThreeDotsVertical } from "react-bootstrap-icons";
 function ArtCard({ item }) {
   const [open, setOpen] = useState(false);
 
-  const { name, description, price } = item;
+  const { name, description, price, category } = item;
   return (
-     
-      <div className="border-bottom">
-        <img
-          alt=""
-          style={{ width: "100%", display: "block" }}
-          height="auto"
-          // background-size="contain"
-           src="http://3.bp.blogspot.com/_EqZzf-l7OCg/TNmdtcyGBZI/AAAAAAAAAD8/KD5Y23c24go/s1600/homer-simpson-1280x1024.jpg"
-        />
+    <div className="border-bottom">
+      <img
+        alt=""
+        style={{ width: "100%", display: "block" }}
+        height="auto"
+        // background-size="contain"
+        src="http://3.bp.blogspot.com/_EqZzf-l7OCg/TNmdtcyGBZI/AAAAAAAAAD8/KD5Y23c24go/s1600/homer-simpson-1280x1024.jpg"
+      />
 
-        <p class="font-weight-light">{name}</p>
-        <Stack direction="horizontal" gap={3}>
-          <div className="ms-auto">
-            {"KES: "}
-            {price}
-          </div>
-          <ThreeDotsVertical onClick={() => setOpen(!open)} />
-        </Stack>
+      <h6 style={{ fontWeight: 200 }}>The sun rises {name}</h6>
+      <p className="font-weight-light" style={{ fontSize: 13 }}>
+        Print{category}
+      </p>
+      <Stack direction="horizontal" gap={2}>
+        <div className="ms-auto"/>
+          <h6 style={{ fontSize: 14, fontWeight: 300 }}>KES{" "}3700{price}</h6>
+          
+        <ThreeDotsVertical onClick={() => setOpen(!open)} />
+      </Stack>
 
-        <Collapse in={open}>
-          <p class="font-italic"> {description}</p>
-        </Collapse>
-      </div>
-   );
+      <Collapse in={open}>
+        <p class="font-italic"> {description}</p>
+      </Collapse>
+    </div>
+  );
 }
 
 ArtCard.propTypes = {
