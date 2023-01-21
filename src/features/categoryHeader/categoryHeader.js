1ca2a8663131241c7b55c7ae92df0fa55ed78ab4
gachagajnr <williampius17@users.noreply.github.com>
2023-01-21 18:17:05 +0300

    configure header custom

--- src/features/categoryHeader/categoryHeader.js
+++ src/features/categoryHeader/categoryHeader.js
@@ -1,20 +1,62 @@
-import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
 import { artCategories } from "constants/artCategories";
 import { Link } from "react-router-dom";
+import { Filter } from "react-bootstrap-icons";
 
 function CategoryHeader() {
   return (
-    <Nav className="justify-content-center" activeKey="/">
-      {artCategories.map((category) => {
-        return (
-          <Nav.Item>
-            <Nav.Link as={Link} to={category.path} >
-              {category.name}
-            </Nav.Link>
-          </Nav.Item>
-        );
-      })}
-    </Nav>
+    <Navbar sticky="top" bg="light" variant="light">
+      <div
+        style={{
+          flex: 1,
+          display: " flex",
+          flexDirection: "column",
+          alignItems: "center",
+          textAlign: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        <div
+          style={{
+            flex: 1,
+            display: " flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            textAlign: "center",
+            flexWrap: "wrap",
+          }}
+        >
+          {artCategories.map((category) => {
+            return (
+              <div style={{ padding: 8 }}>
+                <Link
+                  as={Link}
+                  to={category.path}
+                  style={{
+                    textDecoration: "none",
+                    fontWeight: 400,
+                    fontColor: "grey",
+                  }}
+                >
+                  {category.name}
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+            style={{ width:"auto" }}
+          />
+          <Filter size={35} color="#c1c1c1"/>
+        </Form>
+      </div>
+    </Navbar>
   );
 }
 
